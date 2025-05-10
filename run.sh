588eccd2370f8f95275946198a39a0af04e5a45e
The Ghost of FOSS' Future <163201376+sammylord-holder@users.noreply.github.com>
2025-05-10 16:06:46 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export QDOH_WEB_PORT=9984
-export QDOH_DNS_SERVER="https://dns.nextdns.io/5577fd"
+export QDOH_DNS_SERVER="https://dns.nextdns.io/insertYourID"
 node index.js >/dev/null 2>&1 &
